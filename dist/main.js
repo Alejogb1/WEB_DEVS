6ad15f62b36027818ac1890ae49c19158cfb2a16
Alejo <alejogarb25@gmail.com>
2020-01-06 23:15:50 -0300

    Open and close, side nav functions and function when clicked outside the side nav bar

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,27 @@
 
-  window.onload=function(){
-
-    function classsToggle () {
-        const navs = document.querySelectorAll(".Navbar-items")
+ /* Navbar*/
+ var side = document.getElementById('myNavbar');
+ var sideBarOpen = false;
+ /*Open & Close functions */
+    function openNav() {
+    side.style.width = "250px";
+    side.style.boxShadow = "0 20px 30px 0 rgba(0,0,0,.5)"
+    sideBarOpen = true;
+    document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
+}
     
-        navs.forEach(nav => nav.classList.toggle("Navbar-ToggleShow"));
+    function closeNav() {
+      document.getElementById("myNavbar").style.width = "0px";
+      sideBarOpen = false;
+      document.body.style.backgroundColor = "inherit";
+      side.style.boxShadow = "0"
+}
+
+/* When clicked outside the navbar, close*/
+window.onclick = function(event) {
+  if (sideBarOpen) {
+    if (event.target !== side && event.target !== document.getElementById('openIcon')) {
+      closeNav();
     }
-    document.querySelector(".Navbar-toggle") /* Selecting the menu icon */
-        .addEventListener("click", classsToggle);
-    }
+  }
+}
